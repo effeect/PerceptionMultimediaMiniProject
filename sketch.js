cacc323bafd137826732f790586d0c862f1d01c6
effeect <oliver.dimes3@gmail.com>
2019-01-11 15:53:46 +0000

    Part 1

--- sketch.js
+++ sketch.js
@@ -1,8 +1,4 @@
 /*
-Hello there!
-
-This a template sketch file that you can use to start creating using the power of soundEngine.
-
 There are five main variables you can access that give out values, they spit out values between 0 and 255 :
     - bass
     - lowMid
@@ -10,21 +6,82 @@ There are five main variables you can access that give out values, they spit out
     - highMid
     - treble
     - level (from 0 to 1)
-
-For future reference, the only functions you need for this to function is use "setupSound" in setup and "drawSound" in setup!
-
-If you want examples please look in the examples folder included.
-
-Thanks,
-effeect
 */
+
+var particles = [];
+
+var bassMapped;
+var midMapped;
+var trebleMapped; 
 
 function setup()
 {
     setupSound()
+    createCanvas(500,500)
+    
+    p = new Particle();
 }
 
 function draw()
 {
     drawSound()
+    
+    background(0)
+    
+    var p = new Particle()
+    particles.push(p)
+    
+    for(var i = 0; i < particles.length; i++)
+        {
+            particles[i].show();
+            particles[i].update();
+            
+            if(particles[i].gone())
+                {
+                    particles.splice(i,1)
+                }
+        }
+}
+
+class Particle {
+    
+    constructor() { //This is where all of the variables are intially intialised
+        
+        this.location = createVector(width/2,height/2)
+        this.velocity = createVector(random(-1,1),random(-4,-1))
+        
+        this.gravity = createVector(0,0.05)
+        
+        this.x = width/2
+        this.y = height/2
+        this.vx = random(-1,1)
+        this.vy = random(-5,-1)
+        
+        this.trans = 255;
+    }
+    
+    update()
+    {
+        this.x += this.vx 
+        this.y += this.vy
+        this.trans --
+        
+        this.velocity.add(this.gravity);
+        
+        this.location.add(this.velocity);
+        
+        
+    }
+    
+    gone()
+    {
+        return this.trans < 0
+    }
+    
+    show() {
+        stroke(255)
+        fill(255,random(0,200),0,this.trans)
+        ellipse(this.location.x,this.location.y,16)
+    }
+    
 }
